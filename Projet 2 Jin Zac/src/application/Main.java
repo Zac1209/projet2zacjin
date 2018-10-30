6bab6c9fe18f5a0f5e7d515865c204741335f105
z.chevrier <z.chevrier@7010-442-31.interne.college-gerald-godin.qc.ca>
2018-10-30 16:18:41 -0400

    d

--- Projet 2 Jin Zac/src/application/Main.java
+++ Projet 2 Jin Zac/src/application/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("Bienvenue à la médiathèque");
 			//test
 			//jin was here 2
 		} catch(Exception e) {
