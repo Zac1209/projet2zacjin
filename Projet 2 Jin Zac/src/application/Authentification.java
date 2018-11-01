c88b94f92e671cd537e228aa979c204f1f43a1a2
z.chevrier <z.chevrier@7010-442-31.interne.college-gerald-godin.qc.ca>
2018-11-01 10:22:44 -0400

    Placement des nodes

--- Projet 2 Jin Zac/src/application/Authentification.java
+++ Projet 2 Jin Zac/src/application/Authentification.java
@@ -5,16 +5,22 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Authentification {
@@ -35,9 +41,11 @@ public class Authentification {
 			// VBox Membres du personel
 			VBox vboxMembresPersonnel = new VBox();
 			HBox hboxIdEmployer = new HBox();
+			hboxIdEmployer.setAlignment(Pos.CENTER);
 			Label lblIdEmployer = new Label("No. Employé : ");
 			TextField tfIdEmployer = new TextField();
 			HBox hboxMDPEmployer = new HBox();
+			hboxMDPEmployer.setAlignment(Pos.CENTER);
 			Label lblPasswordEmployer = new Label("Mot de passe: ");
 			PasswordField pfPasswordEmployer = new PasswordField();
 			Button btnConnexionEmployer = new Button("Connexion");
@@ -47,18 +55,26 @@ public class Authentification {
 			
 			//VBox Adhérants
 			VBox vboxAdherants = new VBox();
-			HBox hboxNomEtTelephone = new HBox();
+			GridPane gpAdherants = new GridPane();
 			Label lblNomEtTelephone = new Label("Nom: ");
 			TextField tfNomEtTelephone = new TextField();
-			HBox hboxPrenom = new HBox();
 			Label lblPrenom = new Label("Prénom: ");
 			TextField tfPrenom = new TextField();
+			gpAdherants.setAlignment(Pos.CENTER);
+			ColumnConstraints col1 = new ColumnConstraints();
+			col1.setMinWidth(65);
+			gpAdherants.getColumnConstraints().add(col1);
+				//Régler l'alignement des labels à droite
+			GridPane.setHalignment(lblNomEtTelephone, HPos.RIGHT);
+			GridPane.setHalignment(lblPrenom, HPos.RIGHT);
+			
 			Button btnChangerAuTelephoneOuNom = new Button("Changer le mode de connexion pour le téléphone");
 			btnChangerAuTelephoneOuNom.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent arg0) {
 					NomAfficher = !NomAfficher;
-					hboxPrenom.setVisible(NomAfficher);
+					lblPrenom.setVisible(NomAfficher);
+					tfPrenom.setVisible(NomAfficher);
 					if(NomAfficher == false) {
 						lblNomEtTelephone.setText("Téléphone: ");
 					}else {
@@ -66,10 +82,6 @@ public class Authentification {
 					}
 				}
 			});
-			hboxNomEtTelephone.getChildren().addAll(lblNomEtTelephone,tfNomEtTelephone);
-			hboxPrenom.getChildren().addAll(lblPrenom,tfPrenom);
-			vboxAdherants.getChildren().addAll(hboxNomEtTelephone,hboxPrenom,btnChangerAuTelephoneOuNom);
-			
 			
 			Button btnCatalogue = new Button("Temporaire");
 			btnCatalogue.setOnAction(new EventHandler<ActionEvent>() {
@@ -81,9 +93,15 @@ public class Authentification {
 			    }
 			});
 			
-			vboxAdherants.getChildren().addAll(btnCatalogue);
 			//Ajout des VBox dans celle du login
-		vboxLogin.getChildren().addAll(vboxMembresPersonnel,vboxAdherants);
+			vboxLogin.getChildren().addAll(vboxMembresPersonnel,vboxAdherants,btnCatalogue);
+			
+			//Placement des éléments de la GridPane du login adhérants
+			vboxAdherants.getChildren().addAll(gpAdherants,btnChangerAuTelephoneOuNom);
+			gpAdherants.add(lblNomEtTelephone, 0, 0);
+			gpAdherants.add(tfNomEtTelephone, 1, 0);
+			gpAdherants.add(lblPrenom, 0, 1);
+			gpAdherants.add(tfPrenom, 1, 1);
 	} catch(Exception e) {
 		e.printStackTrace();
 	}
