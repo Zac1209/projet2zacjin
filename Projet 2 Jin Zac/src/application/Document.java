cfdd9189c2ab39a424b665e7276545d275dbcc33
jc.guo <jc.guo@400G4-442-29.interne.college-gerald-godin.qc.ca>
2018-11-01 11:00:54 -0400

    fixed document super sans majuscule

--- Projet 2 Jin Zac/src/application/Document.java
+++ Projet 2 Jin Zac/src/application/Document.java
@@ -1,5 +1,13 @@
 package application;
 
 public abstract class Document {
+	
+	String titre;
+	String auteur;
+	
+	public Document(String titre, String auteur){
+		this.titre = titre;
+		this.auteur = auteur;
+	}
 
 }
--- Projet 2 Jin Zac/src/application/Livre.java
+++ Projet 2 Jin Zac/src/application/Livre.java
@@ -1,6 +1,16 @@
 package application;
 
 public class Livre extends Document{
+	
+	int pages; //testing data not part of actual structure
+	
+	public Livre(String titre, String auteur, int p){
+		
+		super(titre, auteur);
+		pages = p;
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
