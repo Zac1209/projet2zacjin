b6b9335145b94c9a060a7c36ea7ef88abb109d06
z.chevrier <z.chevrier@790-443-03.interne.college-gerald-godin.qc.ca>
2018-11-21 12:54:01 -0500

    fin pause mercredi

--- Projet 2 Jin Zac/src/application/listeDocument.java
+++ Projet 2 Jin Zac/src/application/listeDocument.java
@@ -1,13 +1,46 @@
 package application;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
 public class listeDocument {
 	private static listeDocument instance;
+	static public ArrayList<Document> arListeDoc = new ArrayList<Document>();
 	
-	private listeDocument() {
-		  //Le constructeur to be done
+	private listeDocument() throws IOException {
+		boolean fin = false; 
+		 
+		 
+		FileInputStream fichier = new FileInputStream("document.ser"); 
+		    
+		ObjectInputStream is = new ObjectInputStream(fichier); 
+		Document doc;
+		
+		try {
+			  
+		while(	(doc = (Document) is.readObject())!=null){
+			arListeDoc.add(doc);
+		 
+		}
+		}
+			catch (IOException e) { 
+				
+			   // fin de fichier ou fichier introuvable.  
+			    
+			   
+			} 
+	   
+			catch (ClassNotFoundException e) { 
+		     
+			e.printStackTrace(); 
+			   
+			} 
+		
 	}
 	
-	public static listeDocument getInstance() {
+	public static listeDocument getInstance() throws IOException {
 		  if (instance == null) 
 			  instance = new listeDocument();
 		  return instance; 
