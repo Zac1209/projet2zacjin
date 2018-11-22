9599b32bee1d9b885c09bf0b2bef5fdc446e742c
JC.Guo <JC.Guo@400G4-442-29.interne.college-gerald-godin.qc.ca>
2018-11-22 10:29:31 -0500

    membre

--- Projet 2 Jin Zac/src/application/Membre.java
+++ Projet 2 Jin Zac/src/application/Membre.java
@@ -1,6 +1,13 @@
 package application;
 
 public class Membre {
+	
+	private String nom;
+	private String prenom;
+	private String numTel;//only for adherant
+	private String idPrep;//only for prepose 
+	
+	private boolean estPrepose;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
