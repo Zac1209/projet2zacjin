bae16cd2e898cdb97a7f4f4a3001a52eb346995a
JC.Guo <JC.Guo@790-443-01.interne.college-gerald-godin.qc.ca>
2018-11-23 11:42:48 -0500

    emprunt in classes documents

--- Projet 2 Jin Zac/src/application/DVD.java
+++ Projet 2 Jin Zac/src/application/DVD.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.IOException;
+
 public class DVD extends Document {
 	private int intNbDVD;
 
@@ -11,6 +13,45 @@ public class DVD extends Document {
 
 	public int getIntNbDVD() {
 		return intNbDVD;
+	}
+
+	public String Emprunt(String nom, String prenom, String numTel ) {
+		
+		if(dateEmprunt != null)
+		{
+			int dejaEmprunter = 0;
+			ListeDocument listeComplete;
+			try {
+				listeComplete = ListeDocument.getInstance();
+			
+			
+			for (int i = 0; i < listeComplete.arListeDoc.size() && dejaEmprunter < 2; i++) {
+				if (listeComplete.arListeDoc.get(i) instanceof Periodique)
+					dejaEmprunter++;
+					
+			}
+			
+			if(dejaEmprunter == 2)
+			{
+				return "Vous avez deja emprunter deux dvds";
+			}
+			else
+			{
+				strNomEmprunteur = nom;
+				strPrenomEmprunteur = prenom;
+				strNumeroTelephone = numTel;
+				
+				return "Emprunt fait";
+			}
+			
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return "erreur";
+			}
+		}
+		return "Deja emprunte"; // erase this shit when done
+			
 	}
 
 }
